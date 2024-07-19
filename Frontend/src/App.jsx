2a4d71ecb4cb3f0bab788bdb57eef18a8f1854a3
David-Yanez <luis.yanez@epn.edu.ec>
2024-07-19 17:00:09 -0500

    frontend project start

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App () {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline bg-red-700 text-center'>
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
