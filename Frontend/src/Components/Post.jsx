04a2290d4581b132511d35b2c2f915d5bb3a53d6
ae95caba <ae95caba@gmail.com>
2024-07-23 14:28:46 -0300

    add logic to calculate how old is a post

--- Frontend/src/Components/Post.jsx
+++ Frontend/src/Components/Post.jsx
@@ -1,13 +1,37 @@
 import React from "react";
 import Avatar from "./Avatar";
 export default function Post({ imageUrl, author, date, avatar }) {
+  function getPublicationDate(dateString) {
+    const date = new Date(dateString);
+
+    // Check if the date is valid
+    if (isNaN(date.getTime())) {
+      throw new Error("Invalid date");
+    }
+    const now = new Date();
+    const diff = now - date; // difference in milliseconds
+    console.log(`the diff is ${diff}`);
+    const minutes = Math.floor(diff / (1000 * 60));
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+    if (days > 0) {
+      return `hace ${days} d`;
+    } else if (hours > 0) {
+      return `hace ${hours} h`;
+    } else {
+      return `hace ${minutes} min`;
+    }
+  }
+
   return (
     <div>
-      <div>
+      <div className="flex gap-3 items-center">
         <Avatar imageUrl={avatar} />
-        {author}
-
-        {date}
+        <p>
+          {author}
+          <span className="ml-1">{getPublicationDate(date)}</span>
+        </p>
       </div>
       <img src={imageUrl} alt="" />
     </div>
