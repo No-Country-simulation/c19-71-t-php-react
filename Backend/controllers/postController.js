57b18a616b2939440dae708b6d0d8a7500acc9fe
ae95caba <ae95caba@gmail.com>
2024-07-27 15:51:33 -0300

    add postController create and update middleware

--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -32,6 +32,118 @@ function getBearerHeaderToSetTokenStringOnReq(req, res, next) {
 export const post_create = [
   getBearerHeaderToSetTokenStringOnReq,
   // Validate body and sanitize fields.
+  /*   body("userIdsWhoLiked")
+    .isArray()
+    .custom(async (value) => {
+      if (!Array.isArray(value)) {
+        throw new Error("userIdsWhoLiked must be an array");
+      }
+
+      for (const userId of value) {
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+          throw new Error("Invalid userId format");
+        }
+
+        const user = await User.findById(userId);
+        if (!user) {
+          throw new Error("User not found");
+        }
+      }
+
+      return true;
+    }), */
+  body("imageURL", "Image URL is required")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .custom((value) => {
+      if (!isURL(value)) {
+        throw new Error("Invalid image URL");
+      }
+      return true;
+    })
+    .body("category", "category must be specified")
+    .trim()
+    .isIn(categoryEnum),
+  body("description")
+    .optional() // Allows the field to be absent
+    .trim()
+    .escape(),
+
+  body("createdAt", "Invalid createdAt format").isISO8601(),
+  body("userId", "Invalid userId")
+    .isMongoId() // Basic check for valid ObjectId format
+    .custom(async (value) => {
+      const user = await User.findById(value);
+      if (!user) {
+        throw new Error("User not found");
+      }
+      return true;
+    }),
+  // Process request after validation and sanitization.
+
+  async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    console.log(`errors :${JSON.stringify(errors)}`);
+
+    console.log(`body content is:${JSON.stringify(req.body)}`);
+
+    const post = new Post({
+      userIdsWhoLiked: [],
+      imageURL: req.body.imageURL,
+      category: req.body.category,
+
+      description: req.body?.description,
+      createdAt: req.body.createdAt,
+      userID: req.body.userId,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+
+      res.status(422).json({ error: "Validation failed" });
+      return;
+    } else {
+      try {
+        // Data from form is valid
+        verify(req.token, "secretkey");
+        await post.save();
+        res.status(200).json({ post });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  },
+];
+
+export const post_list = asyncHandler(async (req, res, next) => {
+  const posts = await find().sort({ price: -1 }).exec();
+  console.log(`response is ${JSON.stringify(posts)}`);
+  res.json(posts);
+});
+
+export const post_schema = asyncHandler(async (req, res, next) => {
+  //
+  const schema = _schema.paths;
+  const schemaDetails = Object.keys(schema)
+    .filter((key) => !key.startsWith("_")) // Filter out keys starting with '_'
+    .map((key) => {
+      return {
+        key: key,
+        type: schema[key].instance,
+        required: schema[key].isRequired ? true : false,
+      };
+    });
+  console.log(`schema is ${JSON.stringify(schemaDetails)}`);
+  //
+  res.json(schemaDetails);
+});
+
+export const post_update = [
+  getBearerHeaderToSetTokenStringOnReq,
+  // Validate body and sanitize fields.
   body("userIdsWhoLiked")
     .isArray()
     .custom(async (value) => {
@@ -80,99 +192,16 @@ export const post_create = [
       }
       return true;
     }),
-  // Process request after validation and sanitization.
-
-  async (req, res, next) => {
-    // Extract the validation errors from a request.
-    const errors = validationResult(req);
-
-    console.log(`errors :${JSON.stringify(errors)}`);
-    // Create a BookInstance object with escaped and trimmed data.git
-    console.log(`body content is:${JSON.stringify(req.body)}`);
-
-    const post = new Post({
-      name: decode(req.body.name),
-      price: req.body.price,
-      imgUrl: decode(req.body.imgUrl),
-      outOfStock: req.body.outOfStock,
-      description: req.body.description,
-      flavours: req.body.flavours,
-    });
-
-    if (!errors.isEmpty()) {
-      // There are errors.
-
-      res.status(422).json({ error: "Validation failed" });
-      return;
-    } else {
-      try {
-        // Data from form is valid
-        verify(req.token, "secretkey");
-        await post.save();
-        res.status(200).json({ post });
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  },
-];
-
-export const post_list = asyncHandler(async (req, res, next) => {
-  const posts = await find().sort({ price: -1 }).exec();
-  console.log(`response is ${JSON.stringify(posts)}`);
-  res.json(posts);
-});
-
-export const post_schema = asyncHandler(async (req, res, next) => {
-  //
-  const schema = _schema.paths;
-  const schemaDetails = Object.keys(schema)
-    .filter((key) => !key.startsWith("_")) // Filter out keys starting with '_'
-    .map((key) => {
-      return {
-        key: key,
-        type: schema[key].instance,
-        required: schema[key].isRequired ? true : false,
-      };
-    });
-  console.log(`schema is ${JSON.stringify(schemaDetails)}`);
-  //
-  res.json(schemaDetails);
-});
-
-export const post_update = [
-  getBearerHeaderToSetTokenStringOnReq,
-  // Validate body and sanitize fields.
-  body("name", "name must be specified").trim().isLength({ min: 1 }).escape(),
-
-  body("price", "price must be specified").trim().escape().isNumeric(),
-  body("description")
-    .optional() // Allows the field to be absent
-    .trim()
-    .escape(),
-  body("imgUrl", "imgUrl must be specified")
-    .trim()
-    .isLength({ min: 1 })
-    .escape(),
-  body("outOfStock", "outOfStock must be specified")
-    .trim()
-    .escape()
-    .isBoolean(),
-  body("flavours")
-    .optional() // Allows the field to be absent
-    .trim()
-    .escape()
-    .isNumeric()
-    .withMessage("Flavours must be a valid number"),
 
   async (req, res, next) => {
     const updatedPost = new Post({
-      name: decode(req.body.name),
-      price: req.body.price,
-      imgUrl: decode(req.body.imgUrl),
+      userIdsWhoLiked: req.body.userIdsWhoLiked,
+      imageURL: req.body.imageURL,
+
+      category: req.body.category,
       description: req.body.description,
-      outOfStock: req.body.outOfStock,
-      flavours: req.body.flavours,
+      createdAt: req.body.createdAt,
+      userId: req.body.userId,
       _id: req.params.id, // This is required, or a new ID will be assigned!
     });
 
@@ -185,17 +214,10 @@ export const post_update = [
       try {
         verify(req.token, "secretkey");
         await findByIdAndUpdate(req.params.id, updatedPost, {});
-        //delte missing flavours field
-        if (!updatedPost.flavours) {
-          await findByIdAndUpdate(
-            req.params.id,
-            { $unset: { flavours: 1 } },
-            {}
-          );
-        }
+
         res.status(200).json({});
       } catch (error) {
-        console.log("Error occurred bro:", error);
+        console.log("Error occurred:", error);
         res.status(500).json({ error: error });
       }
     }
